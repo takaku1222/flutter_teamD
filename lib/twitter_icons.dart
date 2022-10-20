1de495604a560a5df5b7842886232eaa9d16567d
Rio Ikeda <ikeda.rio28@gmail.com>
2022-10-20 14:23:59 +0900

    メッセージ画面作成

--- lib/twitter_icons.dart
+++ lib/twitter_icons.dart
@@ -22,9 +22,9 @@ class TI {
   static const String? _kFontPkg = null;
 
   static const IconData home = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData message = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData notification = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData search = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData star = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData tweet = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData message = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData add = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData preferences = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
