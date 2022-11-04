80b8fc796542075c9e672b4dc8a113ddcde55b6f
takaku1222 <tomo15161222@icloud.com>
2022-11-04 15:15:48 +0900

    home

--- lib/src/screen/home.dart
+++ lib/src/screen/home.dart
@@ -27,13 +27,11 @@ class HomePage extends StatelessWidget {
           leading: IconButton(
             icon: const CircleAvatar(
               backgroundImage: NetworkImage(
-                'https://ca.slack-edge.com/T0179KMH83U-U01UFRZGT6C-a1ed61feb4aa-512',
+                'https://ca.slack-edge.com/T0179KMH83U-U01UFRZGT6C-gd57c02093f3-512',
               ),
               radius: 18,
             ),
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
-            },
+            onPressed: () {},
           ),
         ),
         body: SingleChildScrollView(
